8b44bf2babaf71f74e83f9fe627c46b7f42f6b9d
Luis Waesler <waesler@unimess.de>
2021-10-13 15:54:51 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "My First Gatsby",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
